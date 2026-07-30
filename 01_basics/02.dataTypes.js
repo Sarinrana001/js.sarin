53cc0c24548f55edf8e49a71c6d6782eb9cb0b8d
Sarinrana001 <sarinrana1@gmail.com>
2026-07-30 04:13:44 +0000

    datatypes

--- 01_basics/02.dataTypes.js
+++ 01_basics/02.dataTypes.js
@@ -2,3 +2,9 @@
 //alert (3+3)
 console.log(3+3);
 console.log("sarin")
+let name ="sarin"
+let age=13
+let isloggedIn=false
+let sate=null
+console.table([name,age,isloggedIn])
+console.log(typeof null)
