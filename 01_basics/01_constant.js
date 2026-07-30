355e7703aea1af3094d78fdce8d49195b91d809e
Sarinrana001 <sarinrana1@gmail.com>
2026-07-30 02:45:40 +0000

    container

---
+++ 01_basics/01_constant.js
@@ -0,0 +1,11 @@
+const accountId=14453
+let accountEmail="sarin@gmail.com"
+let accountPassword="12345"
+let accountCity="Surkhet"
+let accountState="active"
+//accountId=2
+accountEmail="xya@.com"
+accountPassword="323232323"
+accountCity="npj"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
