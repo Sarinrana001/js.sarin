38eeedf9b572ad09e0df8cb40194fa97a0cf1735
Sarinrana001 <sarinrana1@gmail.com>
2026-07-30 04:35:42 +0000

    coversion of data types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+let isLoggedIn =0
+let booleanIsLogged=Boolean(isLoggedIn)
+console.log(booleanIsLogged);
+let sarin=43
+let stringNumber=String(sarin)
+console.log(stringNumber);
+console.log(typeof stringNumber);
