833145b60b6d773c1af2d670ad4c054593d2792d
Sarinrana001 <sarinrana1@gmail.com>
2026-07-31 03:55:52 +0000

    datatype summary

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+//Primitive 
+
+//7 types :String,Number,Boolean,Null,Undefined,Symbol,BigInt
+ const id=Symbol('123')
+ //const anotherId=Symbol('123')
+ //console.log(id==anotherId);
+    
+
+//Refreence (Nn Primitive)
+//Array,Objets,Functions
+
+const heros=["aaa","bb" ,"ccc"];
+let myObj={
+    name :"ish",
+    age:22,
+
+}
+const myFunction =function(){
+    console.log("hello world");
+
+    
+}
+console.log(typeof heros);
