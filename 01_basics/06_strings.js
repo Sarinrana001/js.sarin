144dded18947b83bd4ec134141997490973cd67d
Sarin Rana <sarinrana1@gmail.com>
2026-08-03 09:04:07 +0545

    Added JavaScript practice files

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,33 @@
+const name="Sarin";
+const email="sarin@example.com";
+const USN="1MS20CS001";
+//console.log(`hello my name is ${name} and my email is ${email} and my USN is ${USN}`);
+const gameName= new String("pubg");
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf("p"));
+const newString=gameName.substring(0,4);
+//console.log(newString);
+const newString1="   sarin   "
+console.log( newString1.trim());
+//const url="https://www.youtube45.com/"
+//console.log(url.replace( '45' , '8'))
+//console.log(url.replace('45','89'))
+const newString3="  name  "
+//console.log(newString3.toUpperCase());
+//console.log(newString3.trim());
+const name1="Sarin"
+const surname="Rana"
+console.log(`my name is ${name} ${surname}`);
+let url="https://www.youtube34.com/watch?v=9ot4CFf0ix8"
+console.log(url.replace('34' ,'45'));
+console.log(url.includes('youtube'))
+
+const gameName2="sarin-hc-com"
+//console.log(gameName2.split('-'));
+console.log(gameName2.bold())
+const newString4=name1.substring(0,4)
+console.log(newString4);
---
+++ 01_basics/07_nums_and _maths.js
@@ -0,0 +1,36 @@
+const score = 400;
+//console.log(score);
+
+const balance = new Number(100);
+//console.log(balance);
+
+//console.log(typeof balance.toString());
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+const otherNumber=50.9090
+
+//console.log(otherNumber.toPrecision(4));
+const hundreds =1005656
+//console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toFixed(1));
+//console.log(hundreds.toLocaleString());
+///////////***************************VV MATHS *//////
+console.log(Math);
+//console.log(Math.abs(-4));      
+//console.log(Math.round(4.6));   
+//console.log(Math.ceil(4.2)); 
+//console.log(Math.floor(4.9));   
+//console.log(Math.abs(-567));
+//console.log(Math.round(56575.676767));
+//console.log(Math.min(3,4,5,6,8));
+////console.log(Math.max(4,5,6,7,8));
+//console.log(Math.random());
+//console.log((Math.random()*10) +1);
+//onsole.log(Math.floor(Math.random()*10)+1);
+const min =10
+const max =20
+console.log(Math.floor(Math.random() *(max-min+1)+min))
+//console.log(Math.round (4.6));
+//console.log(Math.max(5,6,7,8));
+//console.log(Math.fi)
+
