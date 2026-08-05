e9b4936b847e72921348e71efc584076d85d1728
Sarin Rana <sarinrana1@gmail.com>
2026-08-05 09:00:56 +0545

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,27 @@
+ ////Array////
+const marvel_hero =["spiderman","thor","shaktiman","axeman"]
+const dc_hero=["batman" ,"superman","flash","visiom"]
+// marvel_hero.push(dc_hero);
+// console.log(marvel_hero[3][4]);
+
+//  hero=marvel_hero.concat(dc_hero)
+// console.log(hero);
+// const all_new_heros=[...marvel_hero,  ...dc_hero]
+// console.log(all_new_heros);
+const another_array=[1,2,3,4[5,6,7],7]
+
+
+
+console.log(Array.isArray("sarin"));
+console.log(Array.from('"sarin'));
+console.log(Array.from({name:"sarin"}));
+
+
+
+
+let res1=200
+let res2=300
+let res3=400
+console.log(Array.of(res1,res2,res3));
+
+
